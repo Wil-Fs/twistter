3af398cd7ab3e728a6934356991735d39da91183
William <williamfs133@gmail.com>
2024-09-18 10:30:34 -0300

    add: search_profile / search_twists

--- twitter_clone/forms/__init__.py
+++ twitter_clone/forms/__init__.py
@@ -1,3 +1,4 @@
 from .twist_form import TwistForm
 from .user_signup_form import SignUpForm
 from .profile_update_form import ProfilePicForm
+from .update_form_profle import UpdateProfileForm, UpdateDataProfileForm
--- twitter_clone/views/__init__.py
+++ twitter_clone/views/__init__.py
@@ -1,4 +1,4 @@
 from .home import home, twist_like, twist_share, delete_twist, edit_twist, search_twistt, search_user
 from .profile_list import profile_list, unfollow, follow
-from .profile import profile, update_user, followers, follows
+from .profile import profile, update_user, followers, follows, update_user_data
 from .login_screen import login_user, logout_user, register_user
