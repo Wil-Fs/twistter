3681c1becf91d454cff9fa6a6508a139057406a5
William <williamfs133@gmail.com>
2024-09-18 02:01:11 -0300

    add: delete_twist / list follows / list_followers

--- twister/settings.py
+++ twister/settings.py
@@ -133,3 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['https://twistter-production.up.railway.app/', 'https://developtest-production.up.railway.app']
