7586bf253389455feabba70a840e324a31a7e8ad
William <williamfs133@gmail.com>
2024-09-19 11:11:33 -0300

    add: styles

--- twitter_clone/forms/__init__.py
+++ twitter_clone/forms/__init__.py
@@ -1,4 +1,4 @@
 from .twist_form import TwistForm
 from .user_signup_form import SignUpForm
-from .profile_update_form import ProfilePicForm
+from .profile_update_form import ProfilePicForm, ProfileCoverForm
 from .update_form_profle import UpdateProfileForm, UpdateDataProfileForm
